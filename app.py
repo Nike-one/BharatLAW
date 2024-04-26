f2f4027895cf07bb78d26ff9a0645e34c5a8cf06
Nike-one <nikebro2000@gmail.com>
2024-04-26 12:09:05 +0530

    banner loading issue fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="BharatLAW", layout="centered")
 # Display the logo image
 col1, col2, col3 = st.columns([1, 30, 1])
 with col2:
-    st.image("D:/BharatLAW/images/banner.png", use_column_width=True)
+    st.image("https://github.com/Nike-one/BharatLAW/blob/master/images/banner.png", use_column_width=True)
 
 def hide_hamburger_menu():
     st.markdown("""
